d0c6eb422ae2d65b8f15e95c1a32c2b83174df39
DongJun Kim <dev.jun125@gmail.com>
2021-06-07 13:25:04 +0900

    server

---
+++ chatbot_omasang/my_settings.py
@@ -0,0 +1,14 @@
+
+
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME': 'chatbot',                  
+        'USER': 'DONGJUN',                          
+        'PASSWORD': 'dongjun125',                  
+        'HOST': 'kimdongjun.clpoyguhtsh4.ap-northeast-2.rds.amazonaws.com',                     
+        'PORT': '3306',                          
+    }
+}
+
+SECRET_KEY = '$qsc41gj_90#7-@81+m0j*pkkhf_1lh@14@sw1aq8q0k@207k0'
