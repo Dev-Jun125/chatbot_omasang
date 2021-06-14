420c2a85f8b57da896c741904019b8a0f32e3df1
DongJun Kim <dev.jun125@gmail.com>
2021-06-14 09:41:43 +0900

    자연어처리및학습수정

--- chatting/morpheme.py
+++ chatting/morpheme.py
@@ -1,15 +1,14 @@
 from konlpy.tag import Komoran
-import string
 
 class Komo:
     def __init__(self):
         self.komo = Komoran()
 
-    def komo_pos(self, user_input): #평문을 코모란 형태소로 변환합니다.
+    def komo_pos(self, user_input): # 평문을 코모란 형태소로 변환합니다.
         result = self.komo.pos(user_input)
         return result
 
-    def komo_pos_v2(self, user_input, **kwargs): #명사, 형용사, 동사를 추출합니다.
+    def komo_pos_v2(self, user_input, **kwargs): # 명사, 형용사, 동사를 추출합니다.
         raw_list = self.komo.pos(user_input)
         mod_list = []
         for i in range(len(raw_list)):
--- chatting/trainer.py
+++ chatting/trainer.py
@@ -6,45 +6,78 @@ db = dbconnect.SqlCommunication()
 komo = morpheme.Komo()
 
 def yaml_trainer():
-    #try:
-        with open('hanseo_chatbot_corpus.yaml', encoding = 'UTF-8') as corpus:
-            corpus_deployment = yaml.load(corpus, Loader=yaml.FullLoader)
-
-        sql = '''INSERT INTO conversation(input_text, output_text, pos_tags, tagging)VALUES(%s, %s, %s, %s);'''
-        for i in range (int(len(corpus_deployment['Conversations']))):
-            user_question = corpus_deployment['Conversations'][i][0]
-            user_response = corpus_deployment['Conversations'][i][1]
-            converted_question_pos = komo.komo_pos(user_question)
-            converted_question_pos_v2 = komo.komo_pos_v2_str(user_question)
-
-            val = (str(user_question), str(user_response), str(converted_question_pos_v2), str(converted_question_pos))
-
-            db.execute(sql, val)
-            db.commit()
-
-        print('완료.')
-    #except:
-        print("말뭉치 파일을 열 수 없습니다.")
-        exit()
+    with open('hanseo_chatbot_corpus.yaml', encoding = 'UTF-8') as corpus:
+        corpus_deployment = yaml.load(corpus, Loader=yaml.FullLoader)
+    sql = '''INSERT INTO conversation(input_text, output_text, pos_tags, tagging)VALUES(%s, %s, %s, %s);'''
+    for i in range (int(len(corpus_deployment['Conversations']))):
+        user_question = corpus_deployment['Conversations'][i][0]
+        user_response = corpus_deployment['Conversations'][i][1]
+        converted_question_pos = komo.komo_pos(user_question)
+        converted_question_pos_v2 = komo.komo_pos_v2(user_question, state = 'string')
+        val = (str(user_question), str(user_response), str(converted_question_pos_v2), str(converted_question_pos))
+        db.execute(sql, val)
+        db.commit()
+    print('데이터베이스 등록 완료.')
+# yaml_trainer()
+def clear_db():
+    sql = '''TRUNCATE conversation;'''
+    db.execute(sql,None)
+    db.commit()
+    print('초기화 완료.')
 
 
-
-def training():
+def manual_trainer():
     print("학습시킬 질문을 입력하세요. : ",end = '')
     user_question = input()
     converted_question_pos = komo.komo_pos(user_question)
-    converted_question_pos_v2 = komo.komo_pos_v2(user_question)
+    converted_question_pos_v2 = komo.komo_pos_v2(user_question, state = 'string')
     print("학습시킬 답변을 입력하세요. : ",end = '')
     user_response = input()
-    print(converted_question_pos)
+
+    sql = '''INSERT INTO conversation(input_text, output_text, pos_tags, tagging)VALUES(%s, %s, %s, %s)'''
+    val = (str(user_question), str(user_response), str(converted_question_pos_v2), str(converted_question_pos)
+    )
+
+    db.execute(sql, val)
+    db.commit()
     print(converted_question_pos_v2)
 
-    #sql = '''INSERT INTO conversation(input_text, output_text, pos_tags, tagging)VALUES(%s, %s, %s, %s)'''
-    #val = (str(user_question), str(user_response), str(converted_question_pos_v2), str(converted_question_pos)
-    #)
+def cafeteria_update(info):
+    print('학식 정보 DB를 업데이트 합니다.')
+    input_text = '서산 학식 메뉴 뭐야?'
+    sql = '''DELETE FROM chatbot.conversation WHERE input_text = %s'''
+    val = input_text
+    db.execute(sql, val)
 
-    #db.execute(sql, val)
-    #db.commit()
+    converted_question_pos = komo.komo_pos(input_text)
+    converted_question_pos_v2 = komo.komo_pos_v2(input_text, state = 'string')
 
-training()
-    
+    sql = '''INSERT INTO conversation(input_text, output_text, pos_tags, tagging)VALUES(%s, %s, %s, %s)'''
+    val = (input_text, info, str(converted_question_pos_v2), str(converted_question_pos))
+
+    db.execute(sql, val)
+    db.commit()
+    print('학식 업데이트 완료.')
+
+def information_update(info, **kwargs):
+    print('공지사항 정보 DB를 업데이트 합니다.')
+    if kwargs.get('state') == 'collage':
+        input_text = '최신 학사공지 사항 뭐야?'
+    elif kwargs.get('state') == 'general':
+        input_text = '최신 일반공지 사항 뭐야?'
+
+    sql = '''DELETE FROM chatbot.conversation WHERE input_text = %s'''
+    val = input_text
+    db.execute(sql, val)
+ 
+    converted_question_pos = komo.komo_pos(input_text)
+    converted_question_pos_v2 = komo.komo_pos_v2(input_text, state = 'string')
+
+    sql = '''INSERT INTO conversation(input_text, output_text, pos_tags, tagging)VALUES(%s, %s, %s, %s)'''
+    val = (input_text, info, str(converted_question_pos_v2), str(converted_question_pos))
+
+    db.execute(sql, val)
+    db.commit()
+    print('공지사항 업데이트 완료.')
+
+manual_trainer()
