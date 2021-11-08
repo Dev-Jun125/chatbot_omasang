b2daf02bc1756f66c164149fb5c97b36c56b3bf1
DongJun Kim <dev.jun125@gmail.com>
2021-11-08 19:08:22 +0900

    인사말 수정

--- chatting/urls.py
+++ chatting/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
  
 urlpatterns = [
-    path('',views.hello),
+    path('이준현개병신새끼',views.hello),
     path('<word>/', views.basic_Conversation),
     path('test/',views.chatAPI),
 ]
